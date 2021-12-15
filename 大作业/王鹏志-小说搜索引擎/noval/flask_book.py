a2f5071e7ca938c928c1b921cdccbd0763e57990
shenyanjing <2647484467@qq.com>
2021-12-15 11:17:07 +0800

    跳转错误的更改

--- 大作业/王鹏志-小说搜索引擎/noval/flask_book.py
+++ 大作业/王鹏志-小说搜索引擎/noval/flask_book.py
@@ -99,19 +99,33 @@ def login():
 
 
    elif request.method == 'GET':
+      book_names = []
+      jieguo_yesandno = 0
       #接受前端发来的小说名字
-      book_name = request.args.get('search')
-      #获取对应url值
-      myquery = {'book_name':{ "$regex": "^{}".format(book_name)}}
-      mydoc = mycol.find(myquery)
-      url = []
-      for x in mydoc:
-         url.append(x['book_url'])
-      if url:
-         return redirect(url_for('book_url',bookurl = url))
-      else:
+      book_name = request.form['search']
+      #结巴分词处理
+      ss = jieba.cut_for_search(book_name) 
+      ss = (" ".join(ss))
+      ss = ss.split(" ")
+      #匹配书名
+      for x in mycol.find({},{ "_id": 0, "book_name": 1}):
+         for name in ss:
+            jieguo = is_in(name,x['book_name'])
+            if jieguo == False:
+               pass
+            else:
+               book_names.append(jieguo)
+               jieguo_yesandno += 1
+      if jieguo_yesandno == 0:
          return render_template('notfound.html')
-
+      else:
+         url = []
+         for book in book_names:
+            myquery = {'book_name':book}
+            mydoc = mycol.find(myquery)
+            for x in mydoc:
+               url.append(x['book_url'])
+         return redirect(url_for('book_url',bookurl = url))
    else:
       return render_template('notfound.html')
 
